9be42d68e0e5b3f07cb500e5343a6b86b83c275f
Hannah Moran <hannah@Neptune.local>
2022-10-21 13:26:33 -0500

    uploading olympics

---
+++ olympics/queries.sql
@@ -0,0 +1,24 @@
+SELECT DISTINCT noc from noc_regions order by noc;
+
+SELECT DISTINCT athletes.name
+FROM athletes, player_attributes, linking_table
+WHERE athletes.id = linking_table.athletes_id
+AND player_attributes.id = linking_table.player_attributes
+AND player_attributes.team = 'Jamaica'
+ORDER BY athletes.name;
+
+SELECT DISTINCT player_attributes.year, medals.medals, athletes.name
+FROM athletes, player_attributes, medals, linking_table
+WHERE athletes.id = linking_table.athletes_id
+AND player_attributes.id = linking_table.player_attributes
+AND athletes.name LIKE '%Gregory%Efthimios%'
+GROUP BY medals.medals, player_attributes.year, athletes.name
+ORDER BY player_attributes.year, medals.medals;
+
+SELECT player_attributes.team, COUNT(linking_table.medals) as C
+FROM player_attributes, medals, linking_table
+WHERE medals.medals = 'Gold'
+AND medals.id = linking_table.medals
+AND linking_table.player_attributes = player_attributes.id
+GROUP BY player_attributes.team
+ORDER BY C DESC;
